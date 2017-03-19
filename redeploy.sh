2bb78c0fdcacb137e1c195df49bac206e80436fc
mekya <ahmetmermerkaya@gmail.com>
2017-03-19 09:24:44 +0300

    Absolute paths are changed to relative in order to work
    in different environments

--- redeploy.sh
+++ redeploy.sh
@@ -2,13 +2,13 @@
 
 mvn install -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
 
-RED5_DIR=/home/faraklit/softwares/red5-server
+RED5_DIR=../../softwares/red5-server
 
 RED5_LIB_DIR=$RED5_DIR/lib
 
-RED5_JAR=/home/faraklit/git/red5-plus-server-common/target/red5-server-common-1.0.9-M2.jar
+RED5_JAR=./target/red5-server-common-1.0.9-M2.jar
 
-SRC_CONF_DIR=/home/faraklit/git/red5-plus-server/src/main/server/conf/
+SRC_CONF_DIR=../red5-plus-server/src/main/server/conf/
 
 #copy red5 jar from target dir to red5 dir
 cp  $RED5_JAR  $RED5_LIB_DIR/
