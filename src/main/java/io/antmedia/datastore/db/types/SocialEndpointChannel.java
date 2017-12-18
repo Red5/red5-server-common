e0a540386dc741608d051f080e6d0e1eb7f7ed83
mekya <ahmetmermerkaya@gmail.com>
2017-12-18 12:52:38 +0300

    Add social endpoint channel for using structure like Facebook pages

---
+++ src/main/java/io/antmedia/datastore/db/types/SocialEndpointChannel.java
@@ -0,0 +1,25 @@
+package io.antmedia.datastore.db.types;
+
+
+/**
+ * This class is for specifying different channel options in social media. 
+ * It is useful for facebook so far. Type can be user, page, event, group
+ * 
+ * @author mekya
+ *
+ */
+public class SocialEndpointChannel {
+	
+	public SocialEndpointChannel(String accountId, String accountName, String type) {
+		this.id = accountId;
+		this.name = accountName;
+		this.type = type;
+	}
+
+	public String type;
+	
+	public String name;
+	
+	public String id;
+	
+}
