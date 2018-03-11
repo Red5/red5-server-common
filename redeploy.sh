4b68049c229c2922733250b68fa33a2972b6e21a
mekya <ahmetmermerkaya@gmail.com>
2018-03-11 16:19:34 +0300

    Make redeploy.sh script generic for all versions

--- redeploy.sh
+++ redeploy.sh
@@ -7,9 +7,12 @@ RED5_DIR=~/softwares/ant-media-server
 
 RED5_LIB_DIR=$RED5_DIR/lib
 
-RED5_JAR=./target/red5-server-common-1.2.4.jar
+RED5_JAR=./target/red5-server-common.jar
 
 SRC_CONF_DIR=../Ant-Media-Server/src/main/server/conf/
+
+#remove older version
+rm $RED5_LIB_DIR/red5-server-common*
 
 #copy red5 jar from target dir to red5 dir
 cp  $RED5_JAR  $RED5_LIB_DIR/
