54a73ccb1dbf49d4936f3971282d1a21b7443954
mekya <ahmetmermerkaya@gmail.com>
2019-02-15 12:59:30 +0300

    Refactor data store class

--- src/main/java/io/antmedia/datastore/db/IDataStoreFactory.java
+++ src/main/java/io/antmedia/datastore/db/IDataStoreFactory.java
@@ -3,5 +3,5 @@ package io.antmedia.datastore.db;
 public interface IDataStoreFactory {
 	public static final String BEAN_NAME = "dataStoreFactory";
 
-	public IDataStore getDataStore();
+	public DataStore getDataStore();
 }
