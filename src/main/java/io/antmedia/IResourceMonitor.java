3b21865dee26f3357653a12e26920b5b5e3a76f1
burak-58 <burak@antmedia.io>
2019-02-19 21:41:05 +0300

    cpu usage control

---
+++ src/main/java/io/antmedia/IResourceMonitor.java
@@ -0,0 +1,9 @@
+package io.antmedia;
+
+public interface IResourceMonitor {
+	public static final String BEAN_NAME = "resourceMonitor";
+
+	public int getAvgCpuUsage();
+
+	public int getCpuLimit();
+}
