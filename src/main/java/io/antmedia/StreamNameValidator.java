463d809b409d713b0f8819a1a8093743bff8ac58
mekya <ahmetmermerkaya@gmail.com>
2019-07-06 18:11:37 +0300

    Add private constructor

--- src/main/java/io/antmedia/StreamNameValidator.java
+++ src/main/java/io/antmedia/StreamNameValidator.java
@@ -4,8 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StreamNameValidator {
-	private static Pattern namePattern = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+	private static Pattern namePattern = Pattern.compile("[^a-z0-9-_]", Pattern.CASE_INSENSITIVE);
 	
+	private StreamNameValidator() {
+		
+	}
 	public static boolean isStreamNameValid(String name) {
 		Matcher m = namePattern.matcher(name);
 		return !m.find();
