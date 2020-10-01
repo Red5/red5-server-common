d5757cc849c40a810d1e8963c52520b5be7c6f2d
burak-58 <burak@antmedia.io>
2020-10-01 15:36:25 +0300

    new methods in jni

--- src/main/java/org/webrtc/BuiltinAudioDecoderFactoryFactory.java
+++ src/main/java/org/webrtc/BuiltinAudioDecoderFactoryFactory.java
@@ -10,15 +10,33 @@
 
 package org.webrtc;
 
+import java.nio.ByteBuffer;
+
 /**
  * Creates a native {@code webrtc::AudioDecoderFactory} with the builtin audio decoders.
  */
 public class BuiltinAudioDecoderFactoryFactory implements AudioDecoderFactoryFactory {
 	public boolean antMedia = false;
+	private long audioDecoderFactory = -1;
+	
   @Override
   public long createNativeAudioDecoderFactory() {
-    return nativeCreateBuiltinAudioDecoderFactory(antMedia);
+     audioDecoderFactory = nativeCreateBuiltinAudioDecoderFactory(antMedia);
+     return audioDecoderFactory;
+  }
+  
+  public void setBufferAndCallback(ByteBuffer packetBuffer) {
+	  nativeSetBufferAndCallback(this, audioDecoderFactory, packetBuffer);
+  }
+  
+  @CalledByNative void callback(String event) {
+	  System.out.println("\n\n**********************************");
+	  System.out.println(event);
+	  System.out.println("*********************************\n\n");
+
   }
 
   private static native long nativeCreateBuiltinAudioDecoderFactory(boolean ant);
+  private static native void nativeSetBufferAndCallback(BuiltinAudioDecoderFactoryFactory thisObj, long audioDecoderFactory, ByteBuffer byteBuffer);
+
 }
