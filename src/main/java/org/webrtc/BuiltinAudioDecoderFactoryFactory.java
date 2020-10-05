83833cee96e082cce74e179038e893d5e3b79c5b
burak-58 <burak@antmedia.io>
2020-10-05 11:35:12 +0300

    packet receiving

--- src/main/java/org/webrtc/BuiltinAudioDecoderFactoryFactory.java
+++ src/main/java/org/webrtc/BuiltinAudioDecoderFactoryFactory.java
@@ -16,27 +16,46 @@ import java.nio.ByteBuffer;
  * Creates a native {@code webrtc::AudioDecoderFactory} with the builtin audio decoders.
  */
 public class BuiltinAudioDecoderFactoryFactory implements AudioDecoderFactoryFactory {
+
+	public interface AudioPacketListener {
+		public void onAudioPacketData(ByteBuffer data, long timestamp);
+	}
+
+	private AudioPacketListener audioPacketListener;
 	public boolean antMedia = false;
 	private long audioDecoderFactory = -1;
-	
-  @Override
-  public long createNativeAudioDecoderFactory() {
-     audioDecoderFactory = nativeCreateBuiltinAudioDecoderFactory(antMedia);
-     return audioDecoderFactory;
-  }
-  
-  public void setBufferAndCallback(ByteBuffer packetBuffer) {
-	  nativeSetBufferAndCallback(this, audioDecoderFactory, packetBuffer);
-  }
-  
-  @CalledByNative void callback(String event) {
-	  System.out.println("\n\n**********************************");
-	  System.out.println(event);
-	  System.out.println("*********************************\n\n");
+	ByteBuffer buffer = ByteBuffer.allocateDirect(4096);
 
-  }
+	@Override
+	public long createNativeAudioDecoderFactory() {
 
-  private static native long nativeCreateBuiltinAudioDecoderFactory(boolean ant);
-  private static native void nativeSetBufferAndCallback(BuiltinAudioDecoderFactoryFactory thisObj, long audioDecoderFactory, ByteBuffer byteBuffer);
+		System.out.println("buffer size:"+buffer.capacity());
 
+		audioDecoderFactory = nativeCreateBuiltinAudioDecoderFactory(this, antMedia, buffer);
+		return audioDecoderFactory;
+	}
+
+
+	@CalledByNative void callback(String event, int size, long timestamp) {
+		System.out.println("\n\n**********************************");
+
+		System.out.println(event+" size:"+size);
+		buffer.rewind();
+		for (int i = 0; i < size; i++) {
+			System.out.print(buffer.get()+" ");		
+		}
+		System.out.println("*********************************\n\n");
+		byte data[] = new byte[size];
+		buffer.rewind();
+		buffer.get(data, 0, size);
+		audioPacketListener.onAudioPacketData(ByteBuffer.wrap(data), timestamp);
+		
+
+	}
+
+	public void setAudioPacketListener(AudioPacketListener audioPacketListener) {
+		this.audioPacketListener = audioPacketListener;
+	}
+
+	private static native long nativeCreateBuiltinAudioDecoderFactory(BuiltinAudioDecoderFactoryFactory thisObj, boolean ant, ByteBuffer byteBuffer);
 }
