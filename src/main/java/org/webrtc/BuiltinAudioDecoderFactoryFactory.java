46684c4af0ac543c73b104b322adf48616b77a07
burak-58 <burak@antmedia.io>
2020-10-06 12:18:26 +0300

    simplify jni

--- src/main/java/org/webrtc/BuiltinAudioDecoderFactoryFactory.java
+++ src/main/java/org/webrtc/BuiltinAudioDecoderFactoryFactory.java
@@ -22,40 +22,32 @@ public class BuiltinAudioDecoderFactoryFactory implements AudioDecoderFactoryFac
 	}
 
 	private AudioPacketListener audioPacketListener;
-	public boolean antMedia = false;
+	private boolean customDecoder = false;
 	private long audioDecoderFactory = -1;
 	ByteBuffer buffer = ByteBuffer.allocateDirect(4096);
 
 	@Override
 	public long createNativeAudioDecoderFactory() {
-
-		System.out.println("buffer size:"+buffer.capacity());
-
-		audioDecoderFactory = nativeCreateBuiltinAudioDecoderFactory(this, antMedia, buffer);
+		audioDecoderFactory = nativeCreateBuiltinAudioDecoderFactory(this, customDecoder, buffer);
 		return audioDecoderFactory;
 	}
 
 
-	@CalledByNative void callback(String event, int size, long timestamp) {
-		System.out.println("\n\n**********************************");
-
-		System.out.println(event+" size:"+size);
-		buffer.rewind();
-		for (int i = 0; i < size; i++) {
-			System.out.print(buffer.get()+" ");		
-		}
-		System.out.println("*********************************\n\n");
+	@CalledByNative void onAudioPacket(int size, long timestamp) {
 		byte data[] = new byte[size];
 		buffer.rewind();
 		buffer.get(data, 0, size);
 		audioPacketListener.onAudioPacketData(ByteBuffer.wrap(data), timestamp);
-		
-
 	}
 
 	public void setAudioPacketListener(AudioPacketListener audioPacketListener) {
 		this.audioPacketListener = audioPacketListener;
 	}
 
-	private static native long nativeCreateBuiltinAudioDecoderFactory(BuiltinAudioDecoderFactoryFactory thisObj, boolean ant, ByteBuffer byteBuffer);
+	private static native long nativeCreateBuiltinAudioDecoderFactory(BuiltinAudioDecoderFactoryFactory thisObj, boolean customDecoder, ByteBuffer byteBuffer);
+
+
+	public void setCustomDecoder(boolean customDecoder) {
+		this.customDecoder = customDecoder;
+	}
 }
