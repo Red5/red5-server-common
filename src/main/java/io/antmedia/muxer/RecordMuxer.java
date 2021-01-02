20ddb29216ab02b4db1c1521f99066254548b419
mekya <ahmetmermerkaya@gmail.com>
2021-01-02 13:24:32 +0300

    Fix synch issue in high profile delayed video case

--- src/main/java/io/antmedia/muxer/RecordMuxer.java
+++ src/main/java/io/antmedia/muxer/RecordMuxer.java
@@ -85,8 +85,8 @@ public abstract class RecordMuxer extends Muxer {
 
 	protected AVPacket videoPkt;
 	protected int rotation;
-	protected long startTimeInVideoTimebase = 0;
-	protected long startTimeInAudioTimebase = 0;
+//	protected long startTimeInVideoTimebase = 0;
+//	protected long startTimeInAudioTimebase = 0;
 	/**
 	 * By default first video key frame should be checked
 	 * and below flag should be set to true
@@ -511,7 +511,7 @@ public abstract class RecordMuxer extends Muxer {
 		if (!firstKeyFrameReceivedChecked && stream.codecpar().codec_type() == AVMEDIA_TYPE_VIDEO) {
 			//we set start time here because we start recording with key frame and drop the other
 			//setting here improves synch between audio and video
-			setVideoStartTime(pkt.pts());
+			//setVideoStartTime(pkt.pts());
 			int keyFrame = pkt.flags() & AV_PKT_FLAG_KEY;
 			if (keyFrame == 1) {
 				firstKeyFrameReceivedChecked = true;
@@ -554,11 +554,6 @@ public abstract class RecordMuxer extends Muxer {
 		pkt.stream_index(index);
 	}
 
-	private void setVideoStartTime(long time) {
-		if (startTimeInVideoTimebase == 0) {
-			startTimeInVideoTimebase = time;
-		}
-	}
 
 	/**
 	 * {@inheritDoc}
@@ -583,7 +578,6 @@ public abstract class RecordMuxer extends Muxer {
 			int keyFrame = pkt.flags() & AV_PKT_FLAG_KEY;
 			//we set start time here because we start recording with key frame and drop the other
 			//setting here improves synch between audio and video
-			setVideoStartTime(pkt.pts());
 			if (keyFrame == 1) {
 				firstKeyFrameReceivedChecked = true;
 				logger.warn("First key frame received for stream: {}", streamId);
@@ -630,11 +624,9 @@ public abstract class RecordMuxer extends Muxer {
 
 		if (codecType == AVMEDIA_TYPE_AUDIO) 
 		{
-			if (startTimeInAudioTimebase == 0) {
-				startTimeInAudioTimebase = pkt.pts();
-			}
-			pkt.pts(av_rescale_q_rnd(pkt.pts() - startTimeInAudioTimebase, inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-			pkt.dts(av_rescale_q_rnd(pkt.dts() - startTimeInAudioTimebase, inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+
+			pkt.pts(av_rescale_q_rnd(pkt.pts(), inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+			pkt.dts(av_rescale_q_rnd(pkt.dts(), inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
 			
 			
 			int ret = av_packet_ref(tmpPacket , pkt);
@@ -650,8 +642,8 @@ public abstract class RecordMuxer extends Muxer {
 		{
 			// we don't set startTimeInVideoTimebase here because we only start with key frame and we drop all frames 
 			// until the first key frame
-			pkt.pts(av_rescale_q_rnd(pkt.pts() - startTimeInVideoTimebase, inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-			pkt.dts(av_rescale_q_rnd(pkt.dts() - startTimeInVideoTimebase, inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+			pkt.pts(av_rescale_q_rnd(pkt.pts(), inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+			pkt.dts(av_rescale_q_rnd(pkt.dts(), inputTimebase, outputTimebase, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
 			
 			
 			int ret = av_packet_ref(tmpPacket , pkt);
