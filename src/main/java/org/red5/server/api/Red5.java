00afa8f2e836fd86227ef4602465efb0c6c262dc
Paul Gregoire <mondain@gmail.com>
2021-01-27 07:45:02 -0800

    Updates for JDK11

--- src/main/java/org/red5/server/api/Red5.java
+++ src/main/java/org/red5/server/api/Red5.java
@@ -55,12 +55,12 @@ public final class Red5 {
     /**
      * Server version with revision
      */
-    public static final String VERSION = "Red5 Server 1.2.2";
+    public static final String VERSION = "Red5 Server 1.2.6";
 
     /**
      * Server version for fmsVer requests
      */
-    public static final String FMS_VERSION = "RED5/1,2,2,0";
+    public static final String FMS_VERSION = "RED5/1,2,6,0";
 
     /**
      * Server capabilities
