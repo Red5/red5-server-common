45951ca42eba60b62abb5e32b154f5ae3b454de7
mekya <ahmetmermerkaya@gmail.com>
2021-02-11 13:35:57 +0300

    Add codec id parametric

--- src/main/java/io/antmedia/muxer/Muxer.java
+++ src/main/java/io/antmedia/muxer/Muxer.java
@@ -338,7 +338,7 @@ public abstract class Muxer {
 		return false;
 	}
 
-	public boolean addAudioStream(int sampleRate, int channelLayout, int streamIndex) {
+	public boolean addAudioStream(int sampleRate, int channelLayout, int codecId, int streamIndex) {
 		return false;
 	}
 	
